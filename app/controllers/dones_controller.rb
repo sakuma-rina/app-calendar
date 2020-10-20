70d81b76fb77878fee8eecc4fd89d78f667adcfe
sakuma-rina <aa3158148@gmail.com>
2020-10-20 19:58:52 +0900

    タスク完了表示

---
+++ app/controllers/dones_controller.rb
@@ -0,0 +1,22 @@
+class DonesController < ApplicationController
+
+  def create
+    @meeting = Meeting.find(params[:meeting_id])
+    @done = Done.new(done_params)
+    if @done.save
+      redirect_to root_path, notice:'タスクが完了しました'
+    else 
+      redirect_to root_path
+    end
+  end
+
+
+
+  private
+
+  def done_params
+    params.permit(:hidden).merge(meeting_id: @meeting.id)
+  end
+
+  
+end
